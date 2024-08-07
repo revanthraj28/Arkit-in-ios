c57c05af01775ec9eb309f91a03470aebd272400
revanthraj28 <revanthraj135@gmail.com>
2024-08-08 00:36:36 +0530

    Initial Commit

---
+++ Belc CO., LTD./ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Belc CO., LTD.
+//
+//  Created by Revanth Raj on 08/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
